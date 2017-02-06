dda5db456f9925c35ad196ae96baf7656c644e0a
sehuno <eric.sehun.oh@gmail.com>
2017-02-05 20:29:58 -0800

    datetime added in order to keep collection by week of the year in addition to database usage with mongodb

--- eventbrighter.py
+++ eventbrighter.py
@@ -3,8 +3,11 @@ import os
 import re
 import pymongo
 import sys
+import json
+import thread
 import lxml
 import requests
+from datetime import *
 from bs4 import BeautifulSoup
 
 hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -15,10 +18,54 @@ hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML,
    	'Connection': 'keep-alive'
 }
 
+global coll
+
+def dateTime():
+	today = datetime.today()
+	return "Week %s" % today.strftime("%U").lstrip("0")
+
+def connectToDB():
+	try:
+		client = pymongo.MongoClient("localhost", 27017)
+	except:
+		print "Could not connect to MongoDB"
+	db = client.eventbrite
+	global coll
+	coll = db[dateTime()]
+
+	# d = dict((db, [dbection for dbection in client[db].dbection_names()])
+    # for db in client.database_names())
+    # 	print json.dumps(d)
+
+def parse_eventbrite():
+	url = "https://www.eventbrite.com/d/ca--san-jose/events--this-week/"
+	# url = "https://www.eventbrite.com/d/ca--san-jose/events--this-week/?crt=regular&page=43&sort=best"
+	# should also include option of parsing different eventbrite pages (free, paid, different city)
+
+	next_page_exists = True
+	while next_page_exists:
+		html = requests.get(url, headers=hdr)
+		soup = BeautifulSoup(html.text, 'lxml')
+		[Event(event_soup) for event_soup in soup.find_all('div','list-card-v2 l-mar-top-2 js-d-poster')]  # pass soup to event constructor
+		next_page_exists = False
+
 class Event:
+	# def print_self(self):
+		# print self.__dict__
+
 	def __init__(self, soup):
+		global coll
 		self.url = soup['data-share-url'].encode('UTF-8').strip() # event url
-		self.title = soup['data-share-name'].encode('UTF-8').strip() # event title
+
+		ret = coll.find({'url' : self.url })
+		if ret.count() != 0:
+			print "%s: %d" % (self.url, ret.count())
+			return
+
+		try:
+			self.title = soup['data-share-name'].encode('UTF-8').strip() # event title
+		except:
+			self.title = "N/A"
 		try: # event price
 			self.price = soup.a.div.span.string.encode('UTF-8').strip()
 		except:
@@ -40,7 +87,10 @@ class Event:
 		except:
 			self.organizer = "N/A"
 
-		self.date = soup.find('time', 'listing-hero-date')['datetime'] # event date
+		try:
+			self.date = soup.find('time', 'listing-hero-date')['datetime'] # event date
+		except:
+			self.date = "N/A"
 		try:
 			self.time = soup.find('time', 'clrfix').find_all('p')[1].string.encode('UTF-8').strip()	# event time
 			# MUST PERFORM CHECK OF DATE FORMAT TYPE
@@ -52,91 +102,16 @@ class Event:
 			children = soup.find_all('div', 'event-details__data')[1].find_all('p', recursive=False)
 			[self.location.append(child.string.encode('UTF-8').strip()) for child in children[:-1]]
 		except:
-			print "location N/A"
+			print "[ERROR] location N/A for %s" % self.title
 
+		coll.insert_one(self.__dict__)
 		print self.__dict__
 		# other details that might be worth storing: image, description
 		# self.print_self
 
-	def print_self(self):
-		print self.__dict__
-
-# class SBIR_Awardee :
-# 	def __init__(self, link):
-# 		global year_limit_reached
-# 		acceptable_cities = ["San Jose", "Sunnyvale", "Santa Clara", "Los Gatos", "Campbell", "Milpitas", "Santa Cruz", "Mountain View", "Saratoga", "Berkeley", "Los Angeles", "Santa Monica", "Palo"]
-# 		html = requests.get(link, headers=hdr)
-# 		self.url = link
-# 		soup = BeautifulSoup(html.text,"lxml")
-# 		self.title = soup.select("#main-column > div > h1")[0].string.encode('utf-8')
-# 		self.year = int(soup.find_all('div', 'col-sm-3 meta-description')[1].text.strip())
-# 		if self.year < 2015:
-# 			year_limit_reached = True
-# 			return
-# 		self.amount = soup.find_all('div', 'col-sm-4 meta-description')[1].text.encode('utf-8').strip()
-# 		self.company = soup.find('div', 'sbc-name-wrapper').string.encode('utf-8').strip()
-# 		print "Current company: %s" % self.company
-# 		self.address = soup.find('div', 'sbc-address-wrapper').string.encode('utf-8').strip()
-# 		self.contact = soup.find('div', 'award-sub-description').text.encode('utf-8').strip()
-# 		self.abstract = soup.find('div', 'abstract-wrapper').text.encode('utf-8')
-# 		if any(acc_city.lower() in self.address.lower() for acc_city in acceptable_cities):
-# 			awardees.append(self)
-
-# def printToFiles(aList):
-# 	file = open('summary.txt', 'w')
-# 	for i, a in enumerate(awardees):
-# 		file.write("%d %s\n\n" % (i + 1, a.title))
-# 		file.write("\t%s" % a.company)
-# 		file.write("\t%s" % a.amount)
-# 		file.write("\t%d" % a.year)
-# 		file.write("\t%s" % a.address)
-# 		file.write("\n\t%s\n\n" % a.contact)
-# 	for i, a in enumerate(awardees):
-# 		file.write("%d %s\n" % (i + 1, a.title))
-# 		file.write("%s\n\n" % a.abstract)
-# 	file.close()
-
 def main(argv):
-	# print "Build an eventbrite scraper!"
-	# try:
-	#     connection = pymongo.MongoClient()
-	#     print "Connected successfully!!!"
-	# except pymongo.errors.ConnectionFailure, e:
-	#    print "Could not connect to MongoDB: %s" % e
-	# connection
-
-	next_page_exists = True
-
-	url = "https://www.eventbrite.com/d/ca--san-jose/events--this-week/"
-	# url = "https://www.eventbrite.com/d/ca--san-jose/events--this-week/?crt=regular&page=43&sort=best"
-
-	while next_page_exists:
-		html = requests.get(url, headers=hdr)
-		soup = BeautifulSoup(html.text, 'lxml')
-		[Event(event_soup) for event_soup in soup.find_all('div','list-card-v2 l-mar-top-2 js-d-poster')]  # pass soup to event constructor
-
-		next_page_exists = False
-		# next_page_exists = soup.find('a','js-show-next is-disabled')
-		# print next_page_exists
-
-	# db = conn.mydb
-	# global awardees
-	# global year_limit_reached
-	# awardees = []
-	# year_limit_reached = False
-	# # startUrl = "https://www.sbir.gov/sbirsearch/award/all/software?f[0]=im_field_state%3A105809"
-	# startUrl = "https://www.sbir.gov/sbirsearch/award/all/?f[0]=im_field_state%3A105809"
-	#
-	# while not year_limit_reached:
-	# 	html = requests.get(startUrl, headers=hdr)
-	# 	soup = BeautifulSoup(html.text, 'lxml')
-	# 	for is_link in soup.find_all('li','search-result'):
-	# 		SBIR_Awardee("https://www.sbir.gov" + is_link.h3.a['href'])
-	# 		if year_limit_reached:
-	# 			break
-	# 	next_page = soup.find('li', 'next').a['href']
-	# 	startUrl = "https://www.sbir.gov" + next_page
-	# printToFiles(awardees)
+	connectToDB()
+	parse_eventbrite()
 
 if __name__ == '__main__' :
 	main(sys.argv[1:])
